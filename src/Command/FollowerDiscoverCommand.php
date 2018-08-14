ad27da0f7a419025b1ce16b71070c7b13f59d659
Raphaël Lefebvre <raphael@raphaellefebvre.be>
2018-08-14 13:36:45 +0200

    create FollowerDiscoverCommand

---
+++ src/Command/FollowerDiscoverCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Command;
+
+use App\Instagram\Instagram;
+use App\Repository\AccountRepository;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FollowerDiscoverCommand extends Command
+{
+    /**
+     * @var Instagram
+     */
+    private $instagram;
+    /**
+     * @var AccountRepository
+     */
+    private $accountRepository;
+
+    public function __construct(Instagram $instagram, AccountRepository $accountRepository)
+    {
+        parent::__construct();
+
+        $this->instagram = $instagram;
+        $this->accountRepository = $accountRepository;
+    }
+
+    protected function configure()
+    {
+        $this
+            ->setName('follower:discover')
+            ->setDescription('Discover potential followers')
+            ->addArgument(
+                'username',
+                InputArgument::REQUIRED
+            );
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $account = $this->accountRepository->findOneBy(['username' => $input->getArgument('username')]);
+
+        if (!$account) {
+            throw new \InvalidArgumentException('account not found');
+        }
+
+        $crawler = $this->instagram->getAccountCrawler($account);
+        foreach ($crawler->discoverAccountsByAccountId($account->getAccountId()) as $account) {
+            $output->writeln(sprintf(
+                '%s (%s)',
+                $account->getUsername(),
+                $account->getPk()
+            ));
+       }
+    }
+}
